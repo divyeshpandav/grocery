4dbf0aa9198dd962063dabdd2011909e63a9468b
divyeshpandav <divyeshpandav1999@gmail.com>
2024-01-06 03:55:57 +0530

    grocery

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+// Page loading animation
+(function ($) {
+
+    "use strict";
+
+    $(window).on('load', function () {
+        if ($('.cover').length) {
+            $('.cover').parallax({
+                imageSrc: $('.cover').data('image'),
+                zIndex: '1'
+            });
+        }
+
+        $("#preloader").animate({
+            'opacity': '0'
+        }, 600, function () {
+            setTimeout(function () {
+                $("#preloader").css("visibility", "hidden").fadeOut();
+            }, 300);
+        });
+    });
+})(window.jQuery);
